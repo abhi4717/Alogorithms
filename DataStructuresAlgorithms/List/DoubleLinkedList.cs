71ab0da15b1c1e3999e78898017c24b908ce9de3
Abhishek Gopalan <abhi4717@gmail.com>
2012-05-23 09:26:35 +0530

    Adding algorithms project

---
+++ DataStructuresAlgorithms/List/DoubleLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructuresAlgorithms.List
+{
+    class DoubleLinkedList
+    {
+        private DoubleLinkedList objPre;
+        private DoubleLinkedList objNext;
+
+        public string Element{ get; set; }
+        public DoubleLinkedList Previous 
+        {
+            get
+            {
+                return objPre;
+            }
+            set
+            {
+                objPre = value;
+            }
+        }
+
+        public DoubleLinkedList Next
+        {
+            get
+            {
+                return objNext;
+            }
+            set
+            {
+                objNext = value;
+            }
+        }
+
+        public DoubleLinkedList(string strElement,DoubleLinkedList objNext, DoubleLinkedList objPrevious)
+        {
+            Element = strElement;
+            Next = objNext;
+            Previous = objPrevious;
+        }
+    }
+}
